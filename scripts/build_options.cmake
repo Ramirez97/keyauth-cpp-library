5cb5bfc73b4ab623d0c3fd34ef33b3dd83b07234
Roberto A. Ramirez Gonzalez <Ramirez97@users.noreply.github.com>
2023-08-31 02:25:20 -0500

    chore(cmake): Delete build_options
    Does not get used.

--- scripts/build_options.cmake
+++
@@ -1,8 +0,0 @@
-option(GEN_PDB "Generate PDB. Default: OFF" OFF)
-
-if(GEN_PDB)
-    target_compile_options(KeyAuth PRIVATE "$<$<CONFIG:Release>:/Z7>")
-    target_link_options(KeyAuth PRIVATE "$<$<CONFIG:Release>:/DEBUG>")
-    target_link_options(KeyAuth PRIVATE "$<$<CONFIG:Release>:/OPT:REF>")
-    target_link_options(KeyAuth PRIVATE "$<$<CONFIG:Release>:/OPT:ICF>")
-endif()
